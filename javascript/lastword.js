850bbe798315cc3496f1eb42e3384b374b7d9096
yhujinhui <52348335+yhujinhui@users.noreply.github.com>
2020-03-28 01:26:01 +0900

    javascript_03_28

--- javascript/lastword.js
+++ javascript/lastword.js
@@ -2,19 +2,37 @@ var body=document.body;
 var word=document.createElement('div');
 word.textContent='제로초'
 document.body.append(word);
+var form=document.createElement('form');
+document.body.append(form);
 var input=document.createElement('input');
-document.body.append(input);
+form.append(input);
 var btn=document.createElement('button');
 btn.textContent='입력';
-document.body.append(btn);
+form.append(btn);
+var re=document.createElement('div');
+document.body.append(re);
 
-var word1=prompt('제로초')
-var word2
-while(1){
-    word2=prompt(word1);
-    if(word2[0]!==word1[word1.length-1]){
-        alert("끝말잇기 끝")
-        break;
+form.addEventListener('submit',function(evt){
+    evt.preventDefault();//기본동작하지마
+    if(word.textContent[word.textContent.length-1]!==input.value[0]){
+        re.textContent='땡';
+        input.value=null;
+        input.focus();
+    }else{
+        re.textContent='딩동댕';
+        word.textContent=input.value;
+        input.value=null;
+        input.focus();
     }
-    word1=word2;
-}
+});
+
+// var word1=prompt('제로초')
+// var word2
+// while(1){
+//     word2=prompt(word1);
+//     if(word2[0]!==word1[word1.length-1]){
+//         alert("끝말잇기 끝")
+//         break;
+//     }
+//     word1=word2;
+// }
---
+++ javascript/onload.js
@@ -0,0 +1,5 @@
+var hw = document.getElementById('hw');
+hw.addEventListener('click',function(){
+	alert('Hello world');
+})
+// 유지보수의 편의성
