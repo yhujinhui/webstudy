2b5abaacb251589acccb3cabb96c9cf5c2d865e3
yhujinhui <52348335+yhujinhui@users.noreply.github.com>
2020-03-28 21:17:14 +0900

    php_03_28

---
+++ php/number.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>1+1</h1>
+	<?php 
+		echo 1+1;
+		// 출력
+ 	?>
+ 	<h1>2-1</h1>
+ 	<?php 
+ 		print(2-1);
+ 	 ?>
+</body>
+</html>
---
+++ php/string.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="UTF-8">
+	<title>String & String Operator</title>
+</head>
+<body>
+	<?php 
+		echo 'Hello \'w\'orld';
+ 	?>
+ 	<h2>concatenation operator</h2>
+ 	<?php 
+ 		echo "Hello"." world";
+ 	 ?>
+ 	 <h2>String length function</h2>
+ 	 <?php 
+ 	 	echo strlen("Hello world");
+ 	 	// 문자 개수
+ 	  ?>
+
+</body>
+</html>
